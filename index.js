7a4d6b2cc26bb3146474b9927c4eae722a84d08d
Christine Cua <cloudycua@gmail.com>
2019-02-08 02:09:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,4 +6,6 @@ const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
+// returns first letter of currentUser name
+// can also use charAt(0)
 const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
